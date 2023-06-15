1010ac2aff8156f44d637ebf0c2a589a7d3a7b87
Jack Wang <jack0218wang@berkeley.edu>
2023-06-14 23:08:12 -0700

    Flask App Updated

---
+++ app/app.py
@@ -0,0 +1,76 @@
+
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, jsonify, request, render_template
+from selenium import webdriver
+import time
+import re
+ 
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+def get_auction_username():
+    stream_link = "https://www.whatnot.com/live/d1d59656-0246-46da-ae89-6f69a4698e01"
+    driver = webdriver.Chrome()
+    driver.get(stream_link)
+
+    time.sleep(3)
+    lets_go_btn = driver.find_element("xpath", "//button[@class='oUI6p']")
+    lets_go_btn.click()
+
+    time.sleep(1)
+    xmark_btn = driver.find_element("xpath", "//*[local-name()='svg' and @data-icon='xmark']")
+    xmark_btn.click()
+    
+    time.sleep(1)
+    sold_btns = driver.find_elements("xpath", "//h5[@data-cy='Sold']")
+    for btn in sold_btns:
+        if (btn.text != ""):
+            btn.click()
+            break
+    elements = driver.find_elements("xpath", "//div[@data-cy='listing_item']")
+    i = 0
+    while True:
+        elem = elements[i]
+        if (elem.text != ""):
+            recent_sale = elem.text
+            print(elem.text)
+            break
+        i += 1
+    driver.close()
+
+    pattern = r"Sold to:\s+(\w+)"
+    # Find the username using regular expressions
+    match = re.search(pattern, recent_sale)
+
+    if match:
+        username = match.group(1)
+        print("Username:", username)
+    else:
+        print("No match found.")
+
+    return jsonify(username)
+
+
+ 
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+def home_page():
+    example_embed=''
+    return render_template('index.html', embed=example_embed)
+
+@app.route('/backend', methods=["GET"])
+# ‘/’ URL is bound with hello_world() function.
+def backend():
+    if request.method == 'GET':
+        return get_auction_username()
+ 
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
---
+++ app/static/js/main.js
@@ -0,0 +1,300 @@
+/* NBA Wheel Spin */
+let nbaContainer = document.querySelector("#nba-tab-content .container-2");
+let nbaBtn = document.getElementById("nba-spin-btn");
+let nbaTeamsArray = Array.from(document.querySelectorAll("#wheel .nba-team"));
+let nbaNumSelected = 0;
+let nbaCurrTeamIdx = 0;
+
+const nbaTeamsLength = nbaTeamsArray.length;
+nbaTeamsArray[nbaTeamsArray.length] = nbaTeamsArray[0];
+
+function getAuction() {
+  fetch('/backend')
+      .then(function (response) {
+          return response.json(response);
+      }).then(function (text) {
+          console.log('GET response:');
+          console.log(text); 
+      });
+}
+
+function NBASpin() {
+  getAuction();
+  let rotation = 0;
+
+  if (nbaNumSelected === nbaTeamsLength) {
+    document.getElementById("nba-spin-btn").disabled = false;
+    return;
+  }
+
+  let rotateAmount = { value: 0 }; // Create an object to hold the rotateAmount value
+
+  nbaTeamRepCheck(rotateAmount);
+
+  let idx = rotateAmount.value % nbaTeamsLength;
+  // console.log(idx);
+
+  let selectedTeam = nbaTeamsArray[nbaTeamsLength - idx];
+  // console.log("team:", selectedTeam.textContent);
+  nbaTeamsArray[nbaTeamsLength - idx] = "selected";
+  nbaNumSelected += 1;
+
+  let rotationIncrement = 360 / nbaTeamsLength; 
+  rotation = rotationIncrement * rotateAmount.value; 
+  nbaContainer.style.transform = "rotate(" + rotation + "deg)";
+
+  nbaContainer.addEventListener("transitionend", function() {
+    selectedTeam.classList.add("selected");
+    let currTeamInput = document.querySelector("#nba-team" + nbaCurrTeamIdx + " input");
+    setTimeout(function() {
+      currTeamInput.value = " " + selectedTeam.textContent;
+    }, 1000);
+  });
+  nbaCurrTeamIdx += 1;
+}
+
+function nbaTeamRepCheck(rotateAmount) {
+  rotateAmount.value = Math.floor(Math.random() * 300);
+  let idx = rotateAmount.value % nbaTeamsLength;
+
+  let selectedTeam = nbaTeamsArray[nbaTeamsLength - idx];
+
+  if (selectedTeam === "selected") {
+    nbaTeamRepCheck(rotateAmount);
+  }
+}
+
+/* NFL Wheel Spin */
+let nflContainer = document.querySelector("#nfl-tab-content .container-2");
+let nflBtn = document.getElementById("nfl-spin-btn");
+let nflTeamsArray = Array.from(document.querySelectorAll("#wheel .nfl-team"));
+let nflNumSelected = 0;
+let nflCurrTeamIdx = 0;
+
+const nflTeamsLength = nflTeamsArray.length;
+nflTeamsArray[nflTeamsArray.length] = nflTeamsArray[0];
+
+function NFLSpin() {
+  let rotation = 0;
+
+  if (nflNumSelected === nflTeamsLength) {
+    document.getElementById("nfl-spin-btn").disabled = false;
+    return;
+  }
+
+  let rotateAmount = { value: 0 }; 
+
+  nflTeamRepCheck(rotateAmount);
+
+  let idx = rotateAmount.value % nflTeamsLength;
+  // console.log(idx);
+
+  let selectedTeam = nflTeamsArray[nflTeamsLength - idx];
+  // console.log("team:", selectedTeam.textContent);
+  nflTeamsArray[nflTeamsLength - idx] = "selected";
+  nflNumSelected += 1;
+
+  let rotationIncrement = 360 / nflTeamsLength; 
+  rotation = rotationIncrement * rotateAmount.value; 
+  nflContainer.style.transform = "rotate(" + rotation + "deg)";
+
+  
+  nflContainer.addEventListener("transitionend", function() {
+    selectedTeam.classList.add("selected");
+    let currTeamInput = document.querySelector("#nfl-team" + nflCurrTeamIdx + " input");
+    setTimeout(function() {
+      currTeamInput.value = " " + selectedTeam.textContent;
+    }, 1000);
+  });
+  nflCurrTeamIdx += 1;
+}
+
+function nflTeamRepCheck(rotateAmount) {
+  rotateAmount.value = Math.floor(Math.random() * 600);
+  let idx = rotateAmount.value % nflTeamsLength;
+
+  let selectedTeam = nflTeamsArray[nflTeamsLength - idx];
+
+  if (selectedTeam === "selected") {
+    nflTeamRepCheck(rotateAmount);
+  }
+}
+
+/* MLB Wheel Spin */
+let mlbContainer = document.querySelector("#mlb-tab-content .container-2");
+let mlbBtn = document.getElementById("mlb-spin-btn");
+let mlbTeamsArray = Array.from(document.querySelectorAll("#wheel .mlb-team"));
+let mlbNumSelected = 0;
+let mlbCurrTeamIdx = 0;
+
+const mlbTeamsLength = mlbTeamsArray.length;
+mlbTeamsArray[mlbTeamsArray.length] = mlbTeamsArray[0];
+
+function MLBSpin() {
+  let rotation = 0;
+
+  if (mlbNumSelected === mlbTeamsLength) {
+    document.getElementById("mlb-spin-btn").disabled = false;
+    return;
+  }
+
+  let rotateAmount = { value: 0 }; 
+
+  mlbTeamRepCheck(rotateAmount);
+
+  let idx = rotateAmount.value % mlbTeamsLength;
+  // console.log(idx);
+
+  let selectedTeam = mlbTeamsArray[mlbTeamsLength - idx];
+  // console.log("team:", selectedTeam.textContent);
+  mlbTeamsArray[mlbTeamsLength - idx] = "selected";
+  mlbNumSelected += 1;
+
+  let rotationIncrement = 360 / mlbTeamsLength; 
+  rotation = rotationIncrement * rotateAmount.value; 
+  mlbContainer.style.transform = "rotate(" + rotation + "deg)";
+
+  
+  mlbContainer.addEventListener("transitionend", function() {
+    selectedTeam.classList.add("selected");
+    let currTeamInput = document.querySelector("#mlb-team" + mlbCurrTeamIdx + " input");
+    setTimeout(function() {
+      currTeamInput.value = " " + selectedTeam.textContent;
+    }, 1000);
+  });
+  mlbCurrTeamIdx += 1;
+}
+
+function mlbTeamRepCheck(rotateAmount) {
+  rotateAmount.value = Math.floor(Math.random() * 600);
+  let idx = rotateAmount.value % mlbTeamsLength;
+
+  let selectedTeam = mlbTeamsArray[mlbTeamsLength - idx];
+
+  if (selectedTeam === "selected") {
+    mlbTeamRepCheck(rotateAmount);
+  }
+}
+
+/* Selecting Tab */ 
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  this.classList.add('tab-border');
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add('show');
+}
+
+function removeBorder() {
+  tabItems.forEach(item => item.classList.remove('tab-border'))
+}
+
+function removeShow() {
+  tabContentItems.forEach(item => item.classList.remove('show'))
+}
+
+tabItems.forEach(item => item.addEventListener('click', selectItem));
+
+
+/* Wheel Reset Button */
+document.getElementById("reset-btn").addEventListener("click", function() {
+  location.reload();
+});
+
+/* Prevent Submit Button from refreshing the page */
+document.getElementById("my-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  // Perform any additional actions or validation here
+
+  // Optional: You can make an asynchronous request to submit the form data
+  // using JavaScript's Fetch API or XMLHttpRequest.
+
+  // Example using Fetch API:
+  /*
+  fetch("/submit-url", {
+    method: "POST",
+    body: new FormData(event.target)
+  })
+  .then(response => {
+    // Handle the response
+  })
+  .catch(error => {
+    // Handle any errors
+  });
+  */
+});
+
+/* Print Break Sheet */
+document.getElementById("export-btn").addEventListener("click", function() {
+  console.log("Export button clicked");
+
+  // Your data preparation code here
+  const dataRows = [];
+
+  // Retrieve the left table element
+  const activeTab = document.querySelector(".show");
+  const leftTable = activeTab.querySelector("#left-table");
+  const leftRows = leftTable.getElementsByTagName("tr");
+  
+  // Loop through each row of the left table (skipping the first row)
+  for (let i = 1; i < leftRows.length; i++) {
+    const row = leftRows[i];
+
+    // Retrieve the username and team values from the input fields
+    const username = row.cells[0].querySelector("input").value;
+    const team = row.cells[1].querySelector("input").value;
+
+    // Add the username and team values to the dataRows array
+    dataRows.push([username, team]);
+  }
+
+  // Retrieve the right table element
+  const rightTable = activeTab.querySelector("#right-table");
+  const rightRows = rightTable.getElementsByTagName("tr");
+
+  // Loop through each row of the right table (skipping the first row)
+  for (let i = 1; i < rightRows.length; i++) {
+    const row = rightRows[i];
+
+    // Retrieve the username and team values from the input fields
+    const username = row.cells[0].querySelector("input").value;
+    const team = row.cells[1].querySelector("input").value;
+
+    // Add the username and team values to the dataRows array
+    dataRows.push([username, team]);
+  }
+
+  // Create a new workbook and worksheet
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet("Sheet1");
+
+  // Add the title row
+  worksheet.addRow(["Username", "Team"]);
+
+  // Add the data rows
+  dataRows.forEach((row) => {
+    worksheet.addRow(row);
+  });
+
+  // Generate the Excel file
+  workbook.xlsx.writeBuffer()
+    .then(function(buffer) {
+      const blob = new Blob([buffer], { type: "application/octet-stream" });
+      const fileName = "data.xlsx";
+
+      // Create a download link and trigger the download
+      const link = document.createElement("a");
+      link.href = window.URL.createObjectURL(blob);
+      link.download = fileName;
+      link.click();
+
+      console.log("Export completed");
+    })
+    .catch(function(error) {
+      console.error("Export error:", error);
+    });
+});
